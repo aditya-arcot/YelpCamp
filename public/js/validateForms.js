e9e1c79703c6a036b07ce9befc82cb4ca90d3fc6
aditya-arcot <arcot.aditya@utexas.edu>
2023-10-12 01:00:27 -0500

    changes to new review
    removed starability
    added custom svg stars
    added js logic for coloring / validation

--- public/js/validateForms.js
+++ public/js/validateForms.js
@@ -4,6 +4,16 @@
         var forms = document.getElementsByClassName('needs-validation');
         Array.from(forms).forEach(function (form) {
             form.addEventListener('submit', function (event) {
+                const ratings = document.querySelectorAll('.rating')
+                if (ratings.length) {
+                    const selected = document.querySelectorAll('.rating.selected')
+                    if (!selected.length) {
+                        ratings.forEach(rating => {
+                            rating.classList.add('warning')
+                        })
+                    }
+                }
+
                 if (form.checkValidity() === false) {
                     event.preventDefault();
                     event.stopPropagation();
